01ce926d736d5974b751af7343d004b8471592c5
narboy <163820304+narboy@users.noreply.github.com>
2026-08-24 22:49:14 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,11 @@
+// Ganti URL ini dengan URL Web App Google Apps Script Anda.
+// Contoh:
+// const GAS_API_URL = 'https://script.google.com/macros/s/AKfycbxxxxxxxxxxxxxxxx/exec';
+
+const GAS_API_URL = 'https://script.google.com/macros/s/AKfycby5f_SEodLim3OGScZyb69bIPzOOSXcvbmEEONjEVt-TTqswqKmYd3KKbPJPrWTmUvF/exec';
+
+const APP_CONFIG = {
+  storeName: 'UB STORE',
+  currencyLocale: 'id-ID',
+  currencyCode: 'IDR'
+};
